a6e2991e00147196f9e7019f1b7dc6e5d1ba16dd
Pedro Meireles <pedromeireles119@gmail.com>
2025-11-04 18:03:13 -0300

    Update sobre.php

--- projeto1/sobre.php
+++ projeto1/sobre.php
@@ -1,15 +1,26 @@
 <?php
     echo <<<HTML
-        <h1>Sobre Nós</h1>
+        <br>
+        <p></p>
+        <h1>SOBRE NÓS</h1>
         <div class="sobre">
-            <div class="sobre-img-celula">
-            <img src="Imagens/imagem-celula.jpg" alt="Foto da Célula na célula do amigo">
-            </div>
-            <div class="sobre-texto">
-            <h2>Essa é a Célula AMEN, criada em 27 de junho de 2025, com o intuito de levar a palavra de Deus e o amor de Cristo aos jovens e adolescentes que estão ao nosso redor.
-            <br>
-            A Célula AMEN se reúne todas as terças feiras, às 19h, na casa do nosso anfitrião Ricksson Cunha. Durante nossos encontros, compartilhamos momentos de louvor, estudo bíblico e comunhão, fortalecendo nossa fé e amizade.
-            </h2>
+            <div class="sobre-item"></div>
+                <div class="sobre-img-celula">
+                    <img src="Imagens/imagem-celula.jpg" alt="Foto da Célula na célula do amigo">
+                </div>
+                <div class="sobre-texto">
+                    <h2>Essa é a Célula AMEN, criada em 27 de junho de 2025, com o intuito de levar a palavra de Deus e o amor de Cristo aos jovens e adolescentes que estão perdidos aos prazeres deste mundo.
+                    <br>
+                    <p>                                                   </p>
+                    <br>
+                    A Célula AMEN se reúne todas as terças feiras, às 19h, na casa do nosso anfitrião Ricksson Cunha. Durante nossos encontros, compartilhamos momentos de louvor, estudo bíblico e comunhão, fortalecendo nossa fé e amizade, onde tudo isso nos leva a um relacionamento mais profundo com Deus e para que consigamos cada dia mais nos parecermos com Ele.
+                    </h2>
+                </div>
             </div>
         </div>
-    HTML;
+        <h1>NOSSOS LÍDERES</h1>
+ 
+
+</div>
+HTML;
+?>
