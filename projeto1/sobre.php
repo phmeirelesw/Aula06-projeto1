27b21d0add7474e3e22878546916fd251bce58b2
Pedro Meireles <pedromeireles119@gmail.com>
2025-11-04 18:18:27 -0300

    Update sobre.php

--- projeto1/sobre.php
+++ projeto1/sobre.php
@@ -19,8 +19,32 @@
             </div>
         </div>
         <h1>NOSSOS LÍDERES</h1>
- 
+        <section class="bloco-imagens-texto">
 
-</div>
+            <article class="item-bloco">
+                <div class="item-imagem">
+                    <img src="Imagens/imagem-guga.jpg" alt="Descrição da Imagem 1">
+                </div>
+                <div class="item-texto">
+                    <h3>LÍDER</h3>
+                    <p>• Gustavo Mhar</p>
+                    <p>• Tenho 18 anos e faço ADS na UNINASSAU</p>
+                    <p>• Estou na Igreja desde 2023 mas me converti no acampamento de carnaval no início de 2024</p>
+                </div>
+            </article>
+
+            <article class="item-bloco">
+                <div class="item-imagem">
+                    <img src="Imagens/imagem-ph.jpg" alt="Descrição da Imagem 2">
+                </div>
+                <div class="item-texto">
+                    <h3>LÍDER AUXILIAR</h3>
+                    <p>• Pedro Meireles</p>
+                    <p>• Tenho 18 anos e faço Computação na UNIPE e Fisioterapia na UFPB</p>
+                    <p>• Estou na Igreja desde outubro de 2024 e me converti em dezembro de 2024, após um evento da igreja e célula.</p>
+                </div>
+            </article>
+
+        </section>
 HTML;
 ?>
