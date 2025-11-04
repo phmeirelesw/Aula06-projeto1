b01f4cf6b8278fb97a5a236d0ab6dc1de8eb3dce
Pedro Meireles <pedromeireles119@gmail.com>
2025-11-04 17:47:15 -0300

    Update topo.php

--- projeto1/topo.php
+++ projeto1/topo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto 1 - Página Inicial</title>
-    <link rel="stylesheet" href="projeto1.css">
+    <link rel="stylesheet" href="projeto.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
