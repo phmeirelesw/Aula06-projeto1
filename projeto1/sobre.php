4663a08af01e06e8e5f2e105b07d5d6a92b1586b
Pedro Meireles <pedromeireles119@gmail.com>
2025-11-04 17:47:12 -0300

    Update sobre.php

--- projeto1/sobre.php
+++ projeto1/sobre.php
@@ -6,10 +6,10 @@
             <img src="Imagens/imagem-celula.jpg" alt="Foto da Célula na célula do amigo">
             </div>
             <div class="sobre-texto">
-            <h3>Essa é a Célula AMEN, criada em 27 de junho de 2025, com o intuito de levar a palavra de Deus e o amor de Cristo aos jovens e adolescentes que estão ao nosso redor.
+            <h2>Essa é a Célula AMEN, criada em 27 de junho de 2025, com o intuito de levar a palavra de Deus e o amor de Cristo aos jovens e adolescentes que estão ao nosso redor.
             <br>
             A Célula AMEN se reúne todas as terças feiras, às 19h, na casa do nosso anfitrião Ricksson Cunha. Durante nossos encontros, compartilhamos momentos de louvor, estudo bíblico e comunhão, fortalecendo nossa fé e amizade.
-            </h3>
+            </h2>
             </div>
         </div>
     HTML;
