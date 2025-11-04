9c7da72e81dacfc588a6d2197617aa0e29377cbb
Pedro Meireles <pedromeireles119@gmail.com>
2025-11-04 17:37:37 -0300

    Update sobre.php

--- projeto1/sobre.php
+++ projeto1/sobre.php
@@ -1,3 +1,15 @@
 <?php
-    echo "<h1>Sobre Nós</h1>";
-    echo "<p>Bem-vindo à nossa página Sobre.</p>";
+    echo <<<HTML
+        <h1>Sobre Nós</h1>
+        <div class="sobre">
+            <div class="sobre-img-celula">
+            <img src="Imagens/imagem-celula.jpg" alt="Foto da Célula na célula do amigo">
+            </div>
+            <div class="sobre-texto">
+            <h3>Essa é a Célula AMEN, criada em 27 de junho de 2025, com o intuito de levar a palavra de Deus e o amor de Cristo aos jovens e adolescentes que estão ao nosso redor.
+            <br>
+            A Célula AMEN se reúne todas as terças feiras, às 19h, na casa do nosso anfitrião Ricksson Cunha. Durante nossos encontros, compartilhamos momentos de louvor, estudo bíblico e comunhão, fortalecendo nossa fé e amizade.
+            </h3>
+            </div>
+        </div>
+    HTML;
